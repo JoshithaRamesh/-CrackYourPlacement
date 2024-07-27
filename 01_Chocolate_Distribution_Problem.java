e88b916a26e73985978209368983fb3b9811067f
Joshitha R <124449879+JoshithaRamesh@users.noreply.github.com>
2024-07-27 22:20:59 +0530

    Day 2

---
+++ 01_Chocolate_Distribution_Problem.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        long res = Long.MAX_VALUE;
+        Collections.sort(a);
+        for(int i =0; i<n-m+1; i++){
+            int min = a.get(i);
+            int max = a.get(i+m-1);
+            res = (long)Math.min(res, max-min);
+        }
+        return res; 
+    }
+}
