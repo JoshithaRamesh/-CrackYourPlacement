d031b7caea52845bba5905b0f03b52b428e0570e
Joshitha R <124449879+JoshithaRamesh@users.noreply.github.com>
2024-08-02 20:49:27 +0530

    String

---
+++ 49_Group_Anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String word: strs){
+            char a[] = word.toCharArray(); 
+            Arrays.sort(a);
+            String s = new String(a);
+            if(!map.containsKey(s)){
+                map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
